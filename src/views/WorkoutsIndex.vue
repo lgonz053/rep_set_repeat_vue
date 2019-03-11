f0bd778bcc2b2577f5659ff1403780bf4c416398
Leo Gonzalez <lgonz053@gmail.com>
2019-03-11 16:22:05 -0500

    Inital commit. Added Login and Signup

---
+++ src/views/WorkoutsIndex.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="workouts">
+    <h1>{{ workouts }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+export default {
+  data: function() {
+    return {
+      workouts: []
+    };
+  },
+  created: function() {
+    axios.get('/api/workouts')
+      .then(response => {
+        this.workouts = response.data
+      });
+  },
+  methods: {}
+};
+</script>
